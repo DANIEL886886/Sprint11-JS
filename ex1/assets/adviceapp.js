5ac7d9366d7e3ee87ec4f1457d85cad724e46a87
DANIEL886886 <danielparaschiv886@gmail.com>
2024-06-08 02:23:12 +0300

    Advice app finished + voice intergation

--- ex1/assets/adviceapp.js
+++ ex1/assets/adviceapp.js
@@ -1,24 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("btn");
+    
+    const button = document.getElementById("btn1");
     button.addEventListener("click", getAdvice);
+
+    const voiceButton = document.getElementById("btn2");
+    voiceButton.addEventListener("click", speakAdvice);
 
     function getAdvice() {
         fetch("https://api.adviceslip.com/advice")
+            
             .then((response) => response.json())
-            .then((data) => {
 
+            .then((data) => {
                 let adviceData = data;
                 console.log(adviceData);
 
                 const receivedadvice = data.slip.advice;
                 console.log(receivedadvice);
 
-                const receivedid = data.slip.id;                
+                const receivedid = data.slip.id;
                 console.log(receivedid);
-                
-                document.getElementById("1").innerText = "ADVICE" + " # " + receivedid;
+
+                document.getElementById("1").innerText =
+                    "ADVICE" + " # " + receivedid;
                 document.getElementById("2").innerText = receivedadvice;
-            
             });
     }
+
+    function speakAdvice() {
+        const advicemsg = document.getElementById("2").innerText;
+        speak(advicemsg);
+    }
+
 });
+
+const speak = (message) => {
+    VoiceRSS.speech({
+        key: "52bfbd75e1c24955831329926a53c5ed",
+        src: message,
+        hl: "en-us",
+        v: "Nancy",
+        r: 0,
+        c: "mp3",
+        f: "22khz_16bit_stereo",
+        ssml: false,
+    });
+};
