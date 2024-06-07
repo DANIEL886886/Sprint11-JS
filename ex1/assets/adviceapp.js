c2b3ac6ba1a2958c4a0550e9ca8fd4e57e28e5f2
DANIEL886886 <danielparaschiv886@gmail.com>
2024-06-08 00:43:02 +0300

    update adviceapp

--- ex1/assets/adviceapp.js
+++ ex1/assets/adviceapp.js
@@ -6,11 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch("https://api.adviceslip.com/advice")
             .then((response) => response.json())
             .then((data) => {
+                
                 let adviceData = data;
-                const receivedadvice = data.slip.advice;
-                const receivedid = data.slip.id;
                 console.log(adviceData);
+
+                const receivedadvice = data.slip.advice;
                 console.log(receivedadvice);
+
+                const receivedid = data.slip.id;                
                 console.log(receivedid);
                 
                 document.getElementById("1").innerText = "ADVICE" + " # " + receivedid;
