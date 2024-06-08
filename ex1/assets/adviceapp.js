98bb7b654f5eafdd88874872a10057968886c238
DANIEL886886 <danielparaschiv886@gmail.com>
2024-06-08 15:39:01 +0300

    Advice app finished + voice intergation

--- ex1/assets/adviceapp.js
+++ ex1/assets/adviceapp.js
@@ -84,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     function speakAdvice() {
+
         const adviceid = document.getElementById("1").innerText;
         speak(adviceid);
 
@@ -91,6 +92,33 @@ document.addEventListener("DOMContentLoaded", () => {
         setTimeout(function () {
             speak(advicemsg);
         }, 2500);
+
+    // let speech = null;
+
+    // using the windows voice integration in order to stop the voice is click outside voice btn
+    // function speakAdvice() {
+    //     const adviceid = document.getElementById("1").innerText;
+    //     speech = new SpeechSynthesisUtterance(adviceid);
+    //     window.speechSynthesis.speak(speech);
+
+    //     const advicemsg = document.getElementById("2").innerText;
+    //     setTimeout(function () {
+    //         speech = new SpeechSynthesisUtterance(advicemsg);
+    //         window.speechSynthesis.speak(speech);
+    //     }, 2500);
+    
+
+    //     document.addEventListener("click", function (event) {
+    //         if (
+    //             !event.target.classList.contains("btn2") &&
+    //             !event.target.closest(".btn2")
+    //         ) {
+    //             if (speech) {
+    //                 window.speechSynthesis.cancel();
+    //                 speech = null;
+    //             }
+    //         }
+    //     });
 
         // animate the border on JSON response
         const container = document.querySelector(".container");
