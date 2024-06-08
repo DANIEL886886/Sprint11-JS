f39a2929376b4927f9bf4f71813864e64004eab6
DANIEL886886 <danielparaschiv886@gmail.com>
2024-06-09 02:42:20 +0300

    Advice app finished + voice intergation

--- ex1/assets/adviceapp.js
+++ ex1/assets/adviceapp.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     // defined event and function for dice button, get random advice
 
     const button = document.getElementById("btn1");
@@ -19,33 +18,30 @@ document.addEventListener("DOMContentLoaded", () => {
                 const receivedid = data.slip.id;
                 console.log(receivedid);
 
-                document.getElementById("1").innerText = "ADVICE" + " # " + receivedid;
+                document.getElementById("1").innerText =
+                    "ADVICE" + " # " + receivedid;
                 document.getElementById("2").innerText = receivedadvice;
             });
 
-        // animate the border on JSON response
-        const container = document.querySelector(".container");
-        container.classList.add("animate-border-dice");
-        // Remove the class after animation is complete
+        // css colorization of container-border, dice button-border, dice button-background
+        let container = document.querySelector(".container");
+        let dicebutton = document.getElementById("btn1");
+
+        container.classList.add("dice-animation");
+        dicebutton.classList.add("dice-animation");
+
         container.addEventListener("animationend", () => {
-            container.classList.remove("animate-border-dice");
+            container.classList.remove("dice-animation");
+            dicebutton.classList.remove("dice-animation");
         });
-        const dicebutton = document.getElementById("btn1");
-        dicebutton.classList.add("animate-border-dice");
-        dicebutton.addEventListener("animationend", () => {
-            dicebutton.classList.remove("animate-border-dice");
-        });
-        const diceid = document.getElementById("1");
+
+        let diceid = document.getElementById("1");
         diceid.classList.add("diceid-colorize");
         diceid.addEventListener("animationend", () => {
             diceid.classList.remove("diceid-colorize");
         });
-        const dicebtncolor = document.getElementById("btn1");
-        dicebtncolor.classList.add("dicebtn-color");
-        dicebtncolor.addEventListener("animationend", () => {
-            dicebtncolor.classList.remove("dicebtn-color");
-        });
 
+        // code to get a random dice number when dice button is pressed
         const img = document.getElementById("btnimage");
         const staticImages = [
             "assets/1.png",
@@ -59,17 +55,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const randomIndex = Math.floor(Math.random() * staticImages.length);
         img.src = staticImages[randomIndex];
 
+        // code to retrieve original spinning dice.gif
         setTimeout(() => {
             img.src = "assets/dice-game.gif";
         }, 1500);
-            
-    };
+    }
 
     // defined event and function for voice button
-
     const voiceButton = document.getElementById("btn2");
     voiceButton.addEventListener("click", speakAdvice);
 
+    //VOICE API CODE
     const speak = (message) => {
         VoiceRSS.speech({
             key: "52bfbd75e1c24955831329926a53c5ed",
@@ -82,43 +78,49 @@ document.addEventListener("DOMContentLoaded", () => {
             ssml: false,
         });
     };
+    //code for the voice api (limited number of api import data)
+    // function speakAdvice() {
 
+    // const adviceid = document.getElementById("1").innerText;
+    // speak(adviceid);
+
+    // const advicemsg = document.getElementById("2").innerText;
+    // setTimeout(function () {
+    //     speak(advicemsg);
+    // }, 2500);
+
+    //using the windows voice integration in order to stop the voice is click outside voice btn
     function speakAdvice() {
 
+        let speech = null;
+
         const adviceid = document.getElementById("1").innerText;
-        speak(adviceid);
+        // the voice reads also the id only when i putted inside a function like the one with setTimeout()
+        setTimeout(function () {
+            speech = new SpeechSynthesisUtterance(adviceid);
+            window.speechSynthesis.speak(speech);
+        }, 500);
 
         const advicemsg = document.getElementById("2").innerText;
         setTimeout(function () {
-            speak(advicemsg);
+            speech = new SpeechSynthesisUtterance(advicemsg);
+            window.speechSynthesis.speak(speech);
         }, 2500);
 
-        // let speech = null;
-
-        // using the windows voice integration in order to stop the voice is click outside voice btn
-        // function speakAdvice() {
-        //     const adviceid = document.getElementById("1").innerText;
-        //     speech = new SpeechSynthesisUtterance(adviceid);
-        //     window.speechSynthesis.speak(speech);
-
-        //     const advicemsg = document.getElementById("2").innerText;
-        //     setTimeout(function () {
-        //         speech = new SpeechSynthesisUtterance(advicemsg);
-        //         window.speechSynthesis.speak(speech);
-        //     }, 2500);
-        
-
-        //     document.addEventListener("click", function (event) {
-        //         if (
-        //             !event.target.classList.contains("btn2") &&
-        //             !event.target.closest(".btn2")
-        //         ) {
-        //             if (speech) {
-        //                 window.speechSynthesis.cancel();
-        //                 speech = null;
-        //             }
-        //         }
-        //     });
+        // stop function when click outside an object wich doesn`t have the class=".btn2"
+        document.addEventListener("click", function (event) {
+            if (
+                (!event.target.classList.contains("btn2") &&
+                    !event.target.closest(".btn2")) ||
+                event.target.classList.contains("btn1") ||
+                event.target.closest(".btn1")
+            ) {
+                if (speech) {
+                    window.speechSynthesis.cancel();
+                    speech = null;
+                }
+            }
+        });
 
         // animate the border on JSON response
         const container = document.querySelector(".container");
@@ -137,12 +139,6 @@ document.addEventListener("DOMContentLoaded", () => {
         voiceid.addEventListener("animationend", () => {
             voiceid.classList.remove("voiceid-colorize");
         });
-        const voicebtncolor = document.getElementById("btn2");
-        voicebtncolor.classList.add("voicebtn-color");
-        voicebtncolor.addEventListener("animationend", () => {
-            voicebtncolor.classList.remove("voicebtn-color");
-        });
     }
-
 });
 
