79ecdfde88ce780dcac50e45880c1df8df399d84
DANIEL886886 <danielparaschiv886@gmail.com>
2024-06-08 03:23:55 +0300

    Advice app finished + voice intergation

--- ex1/assets/adviceapp.js
+++ ex1/assets/adviceapp.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    
 
     // defined event and function for dice button, get random advice
 
@@ -6,23 +7,40 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener("click", getAdvice);
 
     function getAdvice() {
-        
         fetch("https://api.adviceslip.com/advice")
-            
-        .then((response) => response.json())
+            .then((response) => response.json())
 
-        .then((data) => {
-            let adviceData = data;
-            console.log(adviceData);
+            .then((data) => {
+                let adviceData = data;
+                console.log(adviceData);
 
-            const receivedadvice = data.slip.advice;
-            console.log(receivedadvice);
+                const receivedadvice = data.slip.advice;
+                console.log(receivedadvice);
 
-            const receivedid = data.slip.id;
-            console.log(receivedid);
+                const receivedid = data.slip.id;
+                console.log(receivedid);
 
-            document.getElementById("1").innerText = "ADVICE" + " # " + receivedid;
-            document.getElementById("2").innerText = receivedadvice;
+                document.getElementById("1").innerText =
+                    "ADVICE" + " # " + receivedid;
+                document.getElementById("2").innerText = receivedadvice;
+            });
+        
+        // animate the border on JSON response
+        const container = document.querySelector(".container")
+        container.classList.add("animate-border-dice");
+        // Remove the class after animation is complete
+        container.addEventListener("animationend", () => {
+            container.classList.remove("animate-border-dice");
+        });
+        const dicebutton = document.getElementById("btn1");
+        dicebutton.classList.add("animate-border-dice");
+        dicebutton.addEventListener("animationend", () => {
+            dicebutton.classList.remove("animate-border-dice");
+        });
+        const diceid = document.getElementById("1");
+        diceid.classList.add("diceid-colorize");
+        diceid.addEventListener("animationend", () => {
+            diceid.classList.remove("diceid-colorize");
         });
     }
 
@@ -45,7 +63,6 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     function speakAdvice() {
-
         const adviceid = document.getElementById("1").innerText;
         speak(adviceid);
 
@@ -54,7 +71,24 @@ document.addEventListener("DOMContentLoaded", () => {
             speak(advicemsg);
         }, 2500);
 
+        // animate the border on JSON response
+        const container = document.querySelector(".container");
+        container.classList.add("animate-border-voice");
+        // Remove the class after animation is complete
+        container.addEventListener("animationend", () => {
+            container.classList.remove("animate-border-voice");
+        });
+        const voicebutton = document.getElementById("btn2");
+        voicebutton.classList.add("animate-border-voice");
+        voicebutton.addEventListener("animationend", () => {
+            voicebutton.classList.remove("animate-border-voice");
+        });
+        const voiceid = document.getElementById("1");
+        voiceid.classList.add("voiceid-colorize");
+        voiceid.addEventListener("animationend", () => {
+            voiceid.classList.remove("voiceid-colorize");
+        });
     }
-    
+
 });
 
